ac6e88467aa0e5c189fa7d0e07ec1304a642f8e5
ImJustAPassenger <ferrauto102@gmail.com>
2024-12-02 20:04:42 +0100

    improving UI and creation of login service demo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../constants/config';
+import { LoginService } from '../Services/login.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   logoUrl: string;
   bgUrl: string;
-
-  constructor() {
+  email!: string | null;
+  password!: string | null;
+  
+  constructor (private router: Router, private loginService: LoginService,private toastrService: ToastrService) {
     // Assegna i valori alle proprietà
     this.logoUrl = LOGO_URL;
     this.bgUrl = BG_IMG_URL;
+    this.email = null;
+    this.password = null;
+    
   }
+ngOnInit()
+{
+  if (this.loginService.isLoggedIn)
+    this.router.navigateByUrl("/browse");  
+}
+
+  onSubmit() {
+    if (!this.email || !this.password) {
+      this.toastrService.error('provide email or passord');
+      return;
+    }
+    console.log(this.email, this.password)
+    this.loginService.login(this.email,this.password)
+    this.email=this.password=null;
+    this.toastrService.success("welcome back");
+    this.router.navigateByUrl("/browse");
+  }
+
 }
